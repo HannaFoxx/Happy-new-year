98c22fb4be85b10362a0a15775b24f41bda7a418
hanna <147699410+HannaFoxx@users.noreply.github.com>
2024-01-11 09:29:44 +0100

    up

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,1 +1,17 @@
-console.log ("test")
+
+function step1 () {
+    var step1 = document.getElementById ("step1")
+    var titleh1 = document.getElementById ("titleh1")
+    var step1texth1 = document.getElementById ("step1texth1")
+    var step1textp = document.getElementById ("step1textp")
+
+
+    step1.style.opacity = 1
+    titleh1.style.opacity = 1
+    step1texth1.style.opacity = 1
+    step1textp.style.opacity = 1
+}
+
+function start() {
+    step1()
+}
